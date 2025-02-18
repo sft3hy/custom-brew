bd35e921535c8ce4943c4ae3d86dbbc93a98f4e5
Sam Townsend <sft3hy@virginia.edu>
2025-02-17 22:03:24 -0800

    more deploy friendly with credentials

--- utils/gsheets_utils.py
+++ utils/gsheets_utils.py
@@ -2,12 +2,25 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 import json
-cred = json.loads(os.getenv("GOOGLE_SHEETS_CREDENTIALS"))
+private_key = os.getenv("GOOGLE_SHEETS_PRIVATE_KEY")
+private_key_id = os.getenv("GOOGLE_SHEETS_PRIVATE_KEY_ID")
+client_id = os.getenv("GOOGLE_SHEETS_CLIENT_ID")
 # Define the scope of the API access
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
-# Authenticate using the service account's JSON file
-# creds = ServiceAccountCredentials.from_json_keyfile_name('path/to/your/service-account-file.json', scope)
+cred = {
+  "type": "service_account",
+  "project_id": "cs-autotranslation",
+  "private_key_id": private_key_id,
+  "private_key": private_key,
+  "client_email": "523461412539-compute@developer.gserviceaccount.com",
+  "client_id": client_id,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/523461412539-compute%40developer.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
 creds = ServiceAccountCredentials.from_json_keyfile_dict(cred, scope)
 
 def update_sheet(email: str, frequency: str, topic: str):
